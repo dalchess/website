c5eda92b2975f13032d90d581b5126af536aaa53
chris boudreau <ch734447@dal.ca>
2023-10-04 20:30:55 -0300

    little test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://dalchess.github.io',
+	site: 'https://chrisboudreau1999.github.io',
 	base: '/website',
 });
